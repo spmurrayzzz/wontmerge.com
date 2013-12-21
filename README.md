a70d9f4cb0a64335e9e9f4c2c146f5642914347f
Stephen Murray <stephen.patrick.murray+github@gmail.com>
2013-12-21 11:48:17 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## wontmerge.com
+
+For when words just won't do denying a pull request any justice.
---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express'),
+  app = express(),
+  fs = require('fs'),
+  mustacheExpress = require('mustache-express'),
+  sizeOf = require('image-size'),
+  assetsPath = __dirname + '/assets',
+  images;
+
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/templates');
+app.use('/assets', express.static(assetsPath));
+
+images = fs.readdirSync(assetsPath + '/img');
+
+app.get('/', function(req, res){
+  var rand = Math.round(Math.random() * (images.length - 1)),
+    randomImg = images[rand],
+    size = sizeOf(assetsPath + '/img/' + randomImg);
+    res.render('index.mustache', { image: randomImg, size: size });
+});
+
+app.listen(3000);
